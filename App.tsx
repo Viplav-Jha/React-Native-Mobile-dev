30141b5d54b4ead387aee8f5b44fa8bf0ba662af
Viplav jha <viplavjha1993@gmail.com>
2023-12-10 16:08:00 +0530

    test commit

--- App.tsx
+++ App.tsx
@@ -2,11 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("Hello world")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <Text>Hello World !</Text>
   );
 }
 
