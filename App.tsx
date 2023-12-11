d45c36191ba4fc26d81ad4d19a60c6f29faef9a6
Viplav jha <viplavjha1993@gmail.com>
2023-12-11 22:25:35 +0530

    code added

--- App.tsx
+++ App.tsx
@@ -3,17 +3,27 @@ import { StyleSheet, Text, View ,FlatList} from 'react-native';
 
 export default function App() {
 
-  const days =[1,6]
+  const days =[1,2,3,4,5,6,7,8]
   return (
     <View style={styles.container}>
 
-      <FlatList/>
-      
+<FlatList
+        data={days}
+        contentContainerStyle={styles.content}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <View style={styles.box}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+     
+      />
+{/*       
       {days.map((day)=>(
          <View style={styles.box} key={day}>
          <Text style={styles.text}>{day}</Text>
         </View>
-      ))}
+      ))} */}
     
       <StatusBar style="auto" />
     </View>
@@ -23,15 +33,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap:10,
+   
+  },
+  content:{
+    gap:10
   },
   box:{
     backgroundColor:'#F9EDE3',
-    width:300,
-    height:300,
+    // width:100,
+    // height:100,
+    flex:1,
+    aspectRatio:1,
+
     borderWidth:StyleSheet.hairlineWidth,
     borderColor:'#9b4521',
     borderRadius:20,
