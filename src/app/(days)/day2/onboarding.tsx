19d5184b6919738567be4fc5fcdbf1f9a4428c5d
Viplav jha <viplavjha1993@gmail.com>
2023-12-13 17:47:24 +0530

    style:titile,description

--- src/app/(days)/day2/onboarding.tsx
+++ src/app/(days)/day2/onboarding.tsx
@@ -1,18 +1,28 @@
 import { Stack } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet,SafeAreaView } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function OnboardingScreen() {
   return (
-    <View style={styles.page}>
+    <SafeAreaView style={styles.page}>
       <Stack.Screen options={{ headerShown: false }} />
-      <FontAwesome5 name="people-arrows" size={24} color="#CEF202" />
-      <Text style={styles.title}>Track every transcation</Text>
-      <Text style={styles.description}>
-        Monitor your spending and contribution, ensuring everty pennty align
-        with your family's aspirations
-      </Text>
-    </View>
+      <View style={styles.pageContainer}>
+      <FontAwesome5
+        style={styles.image}
+        name="people-arrows"
+        size={70}
+        color="#CEF202"
+      />
+      <View style={styles.footer}>
+        <Text style={styles.title}>Track every transaction</Text>
+        <Text style={styles.description}>
+          Monitor your spending and contribution, ensuring everty pennty align
+          with your family's aspirations
+        </Text>
+      </View>
+      </View>
+     
+    </SafeAreaView>
   );
 }
 
@@ -22,19 +32,29 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flex: 1,
     backgroundColor: "#15141A",
-    padding: 20,
   },
-  image: {},
+  pageContainer:{
+    flex:1,
+    padding:20,
+  },
+  image: {
+    alignSelf: "center",
+  },
   title: {
     color: "#FDFDFD",
-    fontSize: 30,
+    fontSize: 50,
     fontWeight: "bold",
-    fontFamily: "InterBold",
+    fontFamily: "InterBlack",
     letterSpacing: 1.3,
+    marginVertical:20
   },
   description: {
     color: "gray",
-    fontSize: 18,
+    fontSize: 20,
     fontFamily: "Inter",
+    lineHeight:28,
+  },
+  footer: {
+    marginTop: "auto",
   },
 });
