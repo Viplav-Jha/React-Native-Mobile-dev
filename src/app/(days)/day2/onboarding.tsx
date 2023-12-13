382a3df25a7ca7a959a37c4cadc6bf7fdf615a6f
Viplav jha <viplavjha1993@gmail.com>
2023-12-13 17:23:31 +0530

    icon : verctor icon added

--- src/app/(days)/day2/onboarding.tsx
+++ src/app/(days)/day2/onboarding.tsx
@@ -1,23 +1,40 @@
 import { Stack } from "expo-router";
-import { Text, View,StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function OnboardingScreen() {
-    return (
-        <View style={styles.page}>
-            <Stack.Screen options={{headerShown:false}}/>
-
-              <Text>OnBoarding</Text>
-        </View>
-    )
+  return (
+    <View style={styles.page}>
+      <Stack.Screen options={{ headerShown: false }} />
+      <FontAwesome5 name="people-arrows" size={24} color="#CEF202" />
+      <Text style={styles.title}>Track every transcation</Text>
+      <Text style={styles.description}>
+        Monitor your spending and contribution, ensuring everty pennty align
+        with your family's aspirations
+      </Text>
+    </View>
+  );
 }
 
-const styles =StyleSheet.create({
-   page:{
-    alignItems:'center',
-    justifyContent:'center',
-    flex:1,
-   },
-   image:{},
-   title:{},
-   description:{}
-})
+const styles = StyleSheet.create({
+  page: {
+    // alignItems:'center',
+    justifyContent: "center",
+    flex: 1,
+    backgroundColor: "#15141A",
+    padding: 20,
+  },
+  image: {},
+  title: {
+    color: "#FDFDFD",
+    fontSize: 30,
+    fontWeight: "bold",
+    fontFamily: "InterBold",
+    letterSpacing: 1.3,
+  },
+  description: {
+    color: "gray",
+    fontSize: 18,
+    fontFamily: "Inter",
+  },
+});
