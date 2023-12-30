81876d0eecb2c75a7e5fd8253a6ebd196c67cbf7
Viplav jha <viplavjha1993@gmail.com>
2023-12-30 20:26:43 +0530

    protect index file

---
+++ src/app/(days)/day5/protected/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProtectedScreen = () => {
+  return (
+    <View>
+      <Text> This is private Info </Text>
+    </View>
+  )
+}
+
+export default ProtectedScreen
