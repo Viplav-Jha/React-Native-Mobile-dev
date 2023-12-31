4b01d2ea9e4a2cf4bf2b6de4c5ab72ef64276a7b
Viplav jha <viplavjha1993@gmail.com>
2023-12-31 11:53:01 +0530

    next screen created

---
+++ src/app/(days)/day5/protected/second.tsx
@@ -0,0 +1,31 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Link } from "expo-router";
+
+const ProtectedScreen = () => {
+  return (
+   
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <Text style={{ fontFamily: "Inter", fontSize: 20, marginBottom: 20 }}>
+          More Protected Info
+        </Text>
+        <FontAwesome5
+          name="lock"
+          size={75}
+          color="gray"
+        />
+     <Link href={"/day5/protected/index"} >Prev page</Link>
+      </View>
+    
+  );
+};
+
+export default ProtectedScreen;
