91989d6e98f16791d7f82792dea93bec757ea277
Viplav jha <viplavjha1993@gmail.com>
2024-01-01 14:27:11 +0530

    indentation corrected

--- src/app/components/core/day4/ForcasteItem.tsx
+++ src/app/components/core/day4/ForcasteItem.tsx
@@ -1,31 +1,29 @@
-import { View, Text,StyleSheet } from 'react-native'
-import React from 'react'
-import { WeatherForecast } from '@/app/(days)/day4/weather';
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { WeatherForecast } from "@/app/(days)/day4/weather";
 
-
-const ForcasteItem = ({forcaste}:{forcaste:WeatherForecast}) => {
+const ForcasteItem = ({ forcaste }: { forcaste: WeatherForecast }) => {
   return (
     <View style={styles.container}>
-    <Text style={styles.temp}>{forcaste.main.temp}</Text>
-  </View>
-  )
-}
+      <Text style={styles.temp}>{forcaste.main.temp}</Text>
+    </View>
+  );
+};
 
-const styles =StyleSheet.create({
-   container:{
-    backgroundColor:'red',
-    padding:10,
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "red",
+    padding: 10,
     alignItems: "center",
-    borderRadius:10,
-    alignitem:'center',
-    justifyContent:'center'
+    borderRadius: 10,
+    alignitem: "center",
+    justifyContent: "center",
+  },
 
-   },
-   
-   temp:{
-    fontFamily:'InterBold',
-    fontSize:20,
-    color:'gray'
-   }
-})
+  temp: {
+    fontFamily: "InterBold",
+    fontSize: 20,
+    color: "gray",
+  },
+});
 export default ForcasteItem;
