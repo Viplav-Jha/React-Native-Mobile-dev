9adc95a1477296f7244baffdb83d8133e76a9f8b
Viplav jha <viplavjha1993@gmail.com>
2024-01-01 14:27:21 +0530

    indentation corrected

--- src/app/components/day5/BiometricsProvider.tsx
+++ src/app/components/day5/BiometricsProvider.tsx
@@ -1,49 +1,54 @@
-import { Children, PropsWithChildren, createContext ,useContext, useState} from "react";
-import * as LocalAuthentication from 'expo-local-authentication';
+import {
+  Children,
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+import * as LocalAuthentication from "expo-local-authentication";
 import { Alert } from "react-native";
 
-type BiometricsProvider ={
-    isUnlocked:boolean
-    authenticate: ()=> Promise<void>
- }
+type BiometricsProvider = {
+  isUnlocked: boolean;
+  authenticate: () => Promise<void>;
+};
 const BiometricsContext = createContext<BiometricsProvider>({
-    isUnlocked:false,
-    authenticate:  async ()=>{},
+  isUnlocked: false,
+  authenticate: async () => {},
 });
 
+const BiometricsProvider = ({ children }: PropsWithChildren) => {
+  const [isUnlocked, setIsUnloked] = useState(false);
+  const authenticate = async () => {
+    const hasHardware = await LocalAuthentication.hasHardwareAsync();
+    console.log("Does the device have biometric hardware?", hasHardware);
 
-const BiometricsProvider =({children}:PropsWithChildren) =>{
-    const [isUnlocked,setIsUnloked] = useState(false)
-    const authenticate = async () => {
-        const hasHardware = await LocalAuthentication.hasHardwareAsync();
-        console.log('Does the device have biometric hardware?', hasHardware);
-
-        if(!hasHardware){
-           Alert.alert("Not supported");
-        //    setUnloked(true)  // As per business requrement
-           return;
-        }
-        if (hasHardware) {
-            // Proceed with biometric authentication
-            const res = await LocalAuthentication.authenticateAsync({
-                promptMessage: 'Authenticate to continue',
-                fallbackLabel: 'Enter PIN',
-            });
-            console.log(res);
-            if(res.success){
-                setIsUnloked(true)
-            }
-        } else {
-            console.warn('Device does not have biometric hardware.');
-        }
+    if (!hasHardware) {
+      Alert.alert("Not supported");
+      //    setUnloked(true)  // As per business requrement
+      return;
     }
-    return (
-        <BiometricsContext.Provider value={{isUnlocked,authenticate}}>
-            {children}
-        </BiometricsContext.Provider>
-    )
-}
+    if (hasHardware) {
+      // Proceed with biometric authentication
+      const res = await LocalAuthentication.authenticateAsync({
+        promptMessage: "Authenticate to continue",
+        fallbackLabel: "Enter PIN",
+      });
+      console.log(res);
+      if (res.success) {
+        setIsUnloked(true);
+      }
+    } else {
+      console.warn("Device does not have biometric hardware.");
+    }
+  };
+  return (
+    <BiometricsContext.Provider value={{ isUnlocked, authenticate }}>
+      {children}
+    </BiometricsContext.Provider>
+  );
+};
 
- export default BiometricsProvider;
+export default BiometricsProvider;
 
-  export const useBiometrics = () => useContext(BiometricsContext)
+export const useBiometrics = () => useContext(BiometricsContext);
