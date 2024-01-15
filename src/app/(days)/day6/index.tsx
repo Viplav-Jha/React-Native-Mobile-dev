24770bad5387775e92bc6524c7c8bcaca3f3dcf1
Viplav jha <viplavjha1993@gmail.com>
2024-01-15 18:44:48 +0530

    added scratch code logics

---
+++ src/app/(days)/day6/index.tsx
@@ -0,0 +1,25 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { Link, Stack } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import MarkdownDisplay from '../../../app/components/core/day3/MarkDownDisplay';
+
+const description = `
+# Some thing unique app`;
+
+const DayDetailsScreen = () => {
+  return (
+    <SafeAreaView edges={['bottom']} style={{ flex: 1 }}>
+      <Stack.Screen options={{ title: ' Biometrics' }} />
+
+      <MarkdownDisplay>{description}</MarkdownDisplay>
+
+      <Link href="/day6/home" asChild>
+        <Button title="Go to Unique App" />
+      </Link>
+      
+    </SafeAreaView>
+  );
+};
+
+export default DayDetailsScreen;
