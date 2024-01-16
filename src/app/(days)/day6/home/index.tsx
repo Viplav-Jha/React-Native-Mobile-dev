4734dc329b7ae3380d58622db8a7e3b3cc92e1a0
Viplav jha <viplavjha1993@gmail.com>
2024-01-16 21:34:52 +0530

    index file

--- src/app/(days)/day6/home/index.tsx
+++ src/app/(days)/day6/home/index.tsx
@@ -1,13 +1,12 @@
 // src/App.tsx
 
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import ScratchCard from '../../../components/day6/ScratchCard';
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import ScratchCard from "../../../components/day6/ScratchCard";
 
 const App: React.FC = () => {
   return (
     <SafeAreaView style={styles.container}>
-      
       <ScratchCard winMessage="Congratulations! You won $10" />
     </SafeAreaView>
   );
